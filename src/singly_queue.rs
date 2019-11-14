9ad68bdc898fe81dbe8b299d9dd3e5afec4d2558
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 16:36:45 +0000

    Use Unsafe Rust raw pointers

--- src/singly_queue.rs
+++ src/singly_queue.rs
@@ -1,6 +1,8 @@
-pub struct List<'a, T> {
+use std::ptr;
+
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>, // NEW!
+    tail: *mut Node<T>, // DANGER
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -10,37 +12,51 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     /// Constructor
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None,
+            tail: ptr::null_mut(),
         }
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
             elem: elem,
             // When you push onto the tail, your next is always None
             next: None,
         });
 
         // Put the box in the right place, and then grab a reference to its Node
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                // If the old tail existed, update it to point to the new tail
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_mut().map(|node| &mut **node)
-            }
-            None => {
-                // Otherwise, update the head to point to it
-                self.head = Some(new_tail);
-                self.head.as_mut().map(|node| &mut **node)
-            }
-        };
+        // let new_tail = match self.tail.take() {
+        //     Some(old_tail) => {
+        //         // If the old tail existed, update it to point to the new tail
+        //         old_tail.next = Some(new_tail);
+        //         old_tail.next.as_mut().map(|node| &mut **node)
+        //     }
+        //     None => {
+        //         // Otherwise, update the head to point to it
+        //         self.head = Some(new_tail);
+        //         self.head.as_mut().map(|node| &mut **node)
+        //     }
+        // };
 
-        self.tail = new_tail;
+        // Implement the above using raw pointer.
+        let raw_tail: *mut _ = &mut *new_tail; // Coercion make a raw pointer from a normal pointer
+
+        // .is_null checks for null, equivalent to checking for None
+        if !self.tail.is_null() {
+            // If the old tail existed, update it to point to the new tail
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            // Otherwise, update the head to point to it
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -51,10 +67,53 @@ impl<'a, T> List<'a, T> {
 
             // If we're out of `head`, make sure to set the tail to `None`.
             if self.head.is_none() {
-                self.tail = None;
+                self.tail = ptr::null_mut();
             }
 
             head.elem
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
     }
 }
