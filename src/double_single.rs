f0148eafcba7e08ddb472b19784ec272c398afcc
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 17:10:50 +0000

    Safe stack

--- src/double_single.rs
+++ src/double_single.rs
@@ -2,3 +2,14 @@
 //!
 //! We smash our list into two halves: one going to the left, and one going to the right
 //!
+
+pub struct Stack<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
