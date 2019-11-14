dd6e45eb5750ba56d5e89880b2ae996f8846f533
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 12:32:50 +0000

    Build the list

--- src/deque.rs
+++ src/deque.rs
@@ -25,7 +25,7 @@ struct Node<T> {
 }
 
 impl<T> Node<T> {
-    fn new(elem: T) -> Rc<RefCell<Self>> {
+    pub fn new(elem: T) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Node {
             elem: elem,
             next: None,
@@ -35,10 +35,40 @@ impl<T> Node<T> {
 }
 
 impl<T> List<T> {
-    fn new() -> List<T> {
+    pub fn new() -> List<T> {
         List {
             head: None,
             tail: None,
+        }
+    }
+
+    /// Pushing onto the front of the list.
+    /// It specially handle some boundary cases around empty lists.
+    ///
+    /// Most operations will only touch the head or tail pointer. However when transitioning to or
+    /// from the empty list, it edit both at once.
+    ///
+    /// Each node should have exactly two pointers to it. Each node in the middle of the list is
+    /// pointed at by its predecessor and successor, while the nodes on the ends are pointed to
+    /// by the list itself.
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head -- OK!
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head -- OK!
+            }
         }
     }
 }
