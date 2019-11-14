48a4612ab86379636272d78851f01d747138cd69
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 04:35:37 +0000

    Add peeking

--- src/decent.rs
+++ src/decent.rs
@@ -30,6 +30,14 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 /// We can't drop the contents of the `Box` _after_ deallocating, so there's no way to drop in a tail-recursive manner.
