601f71180a90b706bdc9e304caf739aa2cd34c06
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 17:09:53 +0000

    New mod: double singly-linked list

---
+++ src/double_single.rs
@@ -0,0 +1,4 @@
+//! # Double Singly-Linked List
+//!
+//! We smash our list into two halves: one going to the left, and one going to the right
+//!
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod decent;
 pub mod deque;
+pub mod double_single;
 pub mod minimal;
 pub mod persistent;
 pub mod singly_queue;
