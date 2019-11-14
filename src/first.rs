11471f174631fad6fcf328e5885889bb3dae624e
Cedric Chee <cedric@invictusbyte.com>
2019-11-14 01:45:01 +0000

    Basic data layout

--- src/first.rs
+++ src/first.rs
@@ -1,5 +1,13 @@
-pub enum List {
+pub struct List {
+    head: Link,
+}
+
+enum Link {
     Empty,
-    ElemThenEmpty(i32),
-    ElemThenNotEmpty(i32, Box<List>),
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
 }
